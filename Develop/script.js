270c0c99f29e14e6038b787419f2444098a46943
Gerard Mennella <gerardmennella21@gmail.com>
2022-03-04 02:10:19 -0500

    random character math

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,40 @@
 // Assignment code here
+function generatePassword() {
+    var upperCaseConfirm = window.confirm("Would you like to use Uppercase Letters?")
+    if (upperCaseConfirm) {
+      const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    } else {
+      const upperCase = ""
+    }
+    var lowerCaseConfirm = window.confirm("Would you like to use Lowercase Letters?")
+    if (lowerCaseConfirm) {
+      const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+    } else {
+      const lowerCase = ""
+    }
+    var numbersConfirm = window.confirm("Would you like to use Numbers?")
+    if (numbersConfirm) {
+      const numbers = "0123456789"
+    } else {
+      const numbers = ""
+    }
+    var specialCharConfirm = window.confirm("Would you like to use Special Characters?")
+    if (specialCharConfirm) {
+      const specialChar = "\"\'\@\\!#$%()*+,-./:;<>=?[]^_~{|}`"
+    } else {
+      const specialChar = ""
+    }
 
+    
+    var randomCharacter = function() {
+      return (upperCase + lowerCase + numbers + specialChar)[Math.floor(Math.random() * (upperCase.length + lowerCase.length + numbers.length + specialChar.length))]
+    }
+    
+      
+    randomCharacter()
+    return randomCharacter()
 
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
